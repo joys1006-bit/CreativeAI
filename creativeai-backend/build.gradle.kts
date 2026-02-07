ede4b4dd1a0d568e4e0746912a115f7565120462
joys1006-bit <joys1006.bit@gmail.com>
2026-02-07 23:50:32 +0900

    fix(build): build.gradle.kts 의존성 오류 수정 및 정리

--- creativeai-backend/build.gradle.kts
+++ creativeai-backend/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+﻿plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
     id("org.springframework.boot") version "3.2.2"
@@ -6,7 +6,6 @@ plugins {
 }
 
 group = "com.creativeai"
-
 version = "0.0.1-SNAPSHOT"
 
 java { sourceCompatibility = JavaVersion.VERSION_17 }
@@ -14,49 +13,22 @@ java { sourceCompatibility = JavaVersion.VERSION_17 }
 repositories { mavenCentral() }
 
 dependencies {
-    // Spring WebFlux (�񵿱�/����ŷ)
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-    // Kotlin Coroutines (����Ƽ�� ����)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-
-    // R2DBC & MySQL
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("io.asyncer:r2dbc-mysql:1.0.2")
-
-    // Jackson (JSON)
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    // Kotlin Reflect
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    // Test
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-    testImplementation("io.mockk:mockk:1.13.8")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.withType<Test> { useJUnitPlatform() }
-
-kotlin { jvmToolchain(17) }
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
-
-    // Jackson (JSON)
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    // Kotlin Reflect
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
     testImplementation("io.mockk:mockk:1.13.8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    
-    // Security Test (인증 테스트용)
     testImplementation("org.springframework.security:spring-security-test")
 }
 
